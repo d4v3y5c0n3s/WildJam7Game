6c1b119a8015a7efdb863d5192b253978ffe3723
d4v3y5c0n3s <46783356+d4v3y5c0n3s@users.noreply.github.com>
2019-03-13 12:20:39 -0400

    edited FSM some more

--- person_base.gd
+++ person_base.gd
@@ -15,12 +15,26 @@ extends Position3D
 # -
 
 class StateMachine:
-	var handlers = {}
+	var selfHandlers = {}
 	var startState = null
 	var endStates = []
 	
 	func add_state(name, handler, end_state=0):
-		pass
+		name = name.to_upper()
+		selfHandlers[name] = handler
+		if end_state:
+			endStates.append(name)
+	
+	func set_start(name):
+		startState = name.to_upper()
+	
+	func run(cargo):
+		#  must call .set_start() before .run()
+		var handler = selfHandlers[startState]
+		#  at least one state must be an end_state
+		
+		while true:
+			var newState
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
