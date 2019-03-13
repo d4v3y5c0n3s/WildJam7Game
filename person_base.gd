83f6832fff836382fa3a7714b73701be06397fc6
d4v3y5c0n3s <46783356+d4v3y5c0n3s@users.noreply.github.com>
2019-03-13 08:35:11 -0400

    began creating FSM

--- person_base.gd
+++ person_base.gd
@@ -14,6 +14,14 @@ extends Position3D
 # -thrilled (when character gets inhertance)
 # -
 
+class StateMachine:
+	var handlers = {}
+	var startState = null
+	var endStates = []
+	
+	func add_state(name, handler, end_state=0):
+		pass
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
