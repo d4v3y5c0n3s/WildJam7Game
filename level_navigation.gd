2a98dfe2d4daa0d37936aed0616cc305bdab639c
d4v3y5c0n3s <46783356+d4v3y5c0n3s@users.noreply.github.com>
2019-03-16 17:32:43 -0400

    new changes

--- level_navigation.gd
+++ level_navigation.gd
@@ -1,15 +1,19 @@
 extends Navigation
 
 func getBegin(person_ref):
+	print(get_closest_point(person_ref.get_translation()))
 	return get_closest_point(person_ref.get_translation())
-func getEnd(go_here):
-	return get_closest_point_to_segment((go_here), (go_here * 100))
+#func getEnd(go_here):
+#	print(get_closest_point_to_segment((go_here), (go_here * 100)))
+#	return get_closest_point_to_segment((go_here), (go_here * 100))
 
 func pointGetEnd(go_here):
+	print(get_closest_point(go_here))
 	return get_closest_point(go_here)
 
 func updatePath(p_begin, p_end):
 	var p = get_simple_path(p_begin, p_end, true)
 	var p_path = Array(p)
 	p_path.invert()
+	print(p_path)
 	return p_path
--- person_base.gd
+++ person_base.gd
@@ -1,13 +1,21 @@
 extends Position3D
 
+const IDENTITY = 1#  1 is the identity of people
+
 onready var navNodeRef = get_parent().get_parent()#  gets the navigation node
 
 var state = 0
-var speed = 4.0
+var speed = 6.0
 
 var begin = Vector3()
 var end = Vector3()
 var path = Array()
+
+#deals with states
+var current_action = 0
+var completed_action = true
+
+var b = true#  a helper for calling go_here() only once
 
 func go_here(pos):#  called to set up a path to follow
 	# clears previous values
@@ -16,7 +24,7 @@ func go_here(pos):#  called to set up a path to follow
 	path = Array()
 	
 	begin = navNodeRef.getBegin(self)#  sets up begin
-	end = navNodeRef.getEnd(pos)#  sets up end
+	end = navNodeRef.pointGetEnd(pos)#  sets up end
 	path = navNodeRef.updatePath(begin, end)#  updates the path
 	
 	#  at this point, the path has been set up.
@@ -52,90 +60,93 @@ func go_step(processDelta):
 		
 		t.origin = atPos
 		t = t.looking_at(atPos + atDir, Vector3(0, 1, 0))#  this makes the character face where they are going
-		self.set_transform(t)
+		set_transform(t)
 		
 		if path.size() < 2:
 			path = []
 			#  if this point is reached, our destination has been arrived at
 		
+		return false
 	else:
 		#  this means we are already at our destination
-		pass
+		return true
 
 #  the following calls the above, but uses the Position3Ds 
 #  go_pos_z, go_neg_z, go_neg_x, go_pos_x to just move in
 #  a direction;  could also be used for player control
 func go_up(processDelta):
-	begin = Vector3()
-	end = Vector3()
-	path = Array()
-	
+	print("going up")
 	begin = navNodeRef.getBegin(self)
-	end = navNodeRef.pointGetEnd(Vector3(self.translation.x, self.translation.y, (self.translation.z - 2)))
+	end = navNodeRef.pointGetEnd(Vector3(begin.x, (begin.y + 1), (begin.z - 2)))
 	path = navNodeRef.updatePath(begin, end)
+	print(begin)
+	print(end)
+	print(path)
 	
 	go_step(processDelta)
 
 func go_down(processDelta):
-	begin = Vector3()
-	end = Vector3()
-	path = Array()
-	
+	print("going down")
 	begin = navNodeRef.getBegin(self)
-	end = navNodeRef.pointGetEnd(Vector3(self.translation.x, self.translation.y, (self.translation.z + 2)))
+	end = navNodeRef.pointGetEnd(Vector3(begin.x, (begin.y + 1), (begin.z + 2)))
 	path = navNodeRef.updatePath(begin, end)
+	print(begin)
+	print(end)
+	print(path)
 	
 	go_step(processDelta)
 
 func go_left(processDelta):
-	begin = Vector3()
-	end = Vector3()
-	path = Array()
-	
+	print("going left")
 	begin = navNodeRef.getBegin(self)
-	end = navNodeRef.pointGetEnd(Vector3((self.translation.x + 2), self.translation.y, self.translation.z))
+	end = navNodeRef.pointGetEnd(Vector3((begin.x + 2), (begin.y + 1), begin.z))
 	path = navNodeRef.updatePath(begin, end)
+	print(begin)
+	print(end)
+	print(path)
 	
 	go_step(processDelta)
 
 func go_right(processDelta):
-	begin = Vector3()
-	end = Vector3()
-	path = Array()
-	
+	print("going right")
 	begin = navNodeRef.getBegin(self)
-	end = navNodeRef.pointGetEnd(Vector3((self.translation.x - 2), self.translation.y, self.translation.z))
+	end = navNodeRef.pointGetEnd(Vector3((begin.x - 2), begin.y, begin.z))
 	path = navNodeRef.updatePath(begin, end)
+	print(begin)
+	print(end)
+	print(path)
 	
 	go_step(processDelta)
 
 func _process(delta):
 	
 	#tests directional movement with player input
-	if Input.is_action_pressed("ui_up"):#  up
+	if Input.is_action_just_pressed("ui_up"):#  up
 		go_up(delta)
-	elif Input.is_action_pressed("ui_down"):#  down
+	elif Input.is_action_just_pressed("ui_down"):#  down
 		go_down(delta)
-	elif Input.is_action_pressed("ui_left"):#  left
+	elif Input.is_action_just_pressed("ui_left"):#  left
 		go_left(delta)
-	elif Input.is_action_pressed("ui_right"):#  right
+	elif Input.is_action_just_pressed("ui_right"):#  right
 		go_right(delta)
-	
-	#deals with states
-	var current_action = 0
-	var completed_action = true
 	
 	match state:
 		0:#  wander state
 			if completed_action:#  if there is no current action, randomly choose one
-				current_action = randi() % 3 + 1#  chooses a random action
+				current_action = 2#randi() % 3 + 1#  chooses a random action
+				completed_action = false
 			else:
 				match current_action:
-					1:
+					1:#  pick up an item
 						pass
-					2:
-						pass
-					3:
+					2:#  go to a random room
+						if b:
+							go_here(go_to_random_room())
+							b = false
+						if go_step(delta):
+							completed_action = true
+							b = true
+					3:#  check the will
 						pass
 		1:#  jealous state
 			pass
@@ -150,5 +161,63 @@ func _process(delta):
 		6:#  dead state
 			pass
 
+#  THESE FUNCTIONS CHANGE THE CHARACTER STATE
+func change_to_wander():
+	pass
+func change_to_jealous():
+	pass
+func change_to_angered():
+	pass
+func change_to_fearful():
+	pass
+func change_to_hostile():
+	pass
+func change_to_thrilled():
+	pass
+func change_to_dead():
+	pass
+
+func go_to_random_room():
+	var room_id = randi() % 8 + 1
+	print("-  room_id  -")
+	print(room_id)
+	match room_id:
+		1:#  ground hall
+			return Vector3(0, 7, 0)
+		2:#  guest room
+			return Vector3(38.808567, 7, -1.947572)
+		3:#  2nd floor hall
+			return Vector3(-29.28244, 23, 0)
+		4:#  bathroom
+			return Vector3(2.174207, 23, -1.872252)
+		5:#  2nd floor stairs
+			return Vector3(34.549664, 23, -1.41005)
+		6:#  3rd floor hall
+			return Vector3(16.354214, 39, -1.653817)
+		7:#  bedroom
+			return Vector3(-36.17395, 39, -2.304301)
+		8:
+			return Vector3(2.029072, 55, -3.927081)
+
 func _ready():
 	pass
+
+func _on_encounter_area_entered(area):
+	if area.IDENTITY == 0:#  a room
+		area.people.append(self)#  lets the room know that they have entered
+	elif area.IDENTITY == 1:#  a person
+		pass
+	elif area.IDENTITY == 2:#
+		pass
+	elif area.IDENTITY == 3:#
+		pass
+
+func _on_encounter_area_exited(area):
+	if area.IDENTITY == 0:#  a room
+		area.people.erase(self)#  lets the room know that they have left
+	elif area.IDENTITY == 1:#  a person
+		pass
+	elif area.IDENTITY == 2:#
+		pass
+	elif area.IDENTITY == 3:#
+		pass
