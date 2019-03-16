96de2cb1ef53e01ecea8413136c397eb2627e445
d4v3y5c0n3s <46783356+d4v3y5c0n3s@users.noreply.github.com>
2019-03-15 21:19:16 -0400

    Now, navigation is finished.  Yay!

--- level_navigation.gd
+++ level_navigation.gd
@@ -1,106 +1,15 @@
 extends Navigation
 
-const SPEED = 4.0
+func getBegin(person_ref):
+	return get_closest_point(person_ref.get_translation())
+func getEnd(go_here):
+	return get_closest_point_to_segment((go_here), (go_here * 100))
 
-var camrot  = 0.0
+func pointGetEnd(go_here):
+	return get_closest_point(go_here)
 
-var begin = Vector3()
-var end = Vector3()
-var m = SpatialMaterial.new()#  the material for
-
-var path = []
-var draw_path = true
-
-#  --  positioning guide  --  #
-# 1 block is 4 units wide
-
-func _process(delta):
-
-	if (path.size() > 1):
-		var to_walk = delta * SPEED
-		var to_watch = Vector3(0, 1, 0)
-
-		while (to_walk > 0 and path.size() >= 2):
-			var pfrom = path[path.size() - 1]
-			var pto = path[path.size() - 2]
-			to_watch = (pto - pfrom).normalized()
-			var d = pfrom.distance_to(pto)
-			if (d <= to_walk):
-				path.remove(path.size() - 1)
-				to_walk -= d
-			else:
-				path[path.size() - 1] = pfrom.linear_interpolate(pto, to_walk/d)
-				to_walk = 0
-
-		var atpos = path[path.size() - 1]
-		var atdir = to_watch
-		atdir.y = 0
-
-		var t = Transform()
-		t.origin = atpos
-		t = t.looking_at(atpos + atdir, Vector3(0, 1, 0))
-		get_node("person_base").set_transform(t)
-
-		if (path.size() < 2):
-			path = []
-			set_process(false)
-	else:
-
-		print("didn't go into if statement, ended process")
-
-		set_process(false)
-
-func _update_path():
-
-	#  the issue is here, in _update_path().
-	#  the character follows the path, but it seems
-	#  to be confined to one tile;  troubleshoot this
-
-	print("called _update_path()")
-
-	var p = get_simple_path(begin, end, true)
-	path = Array(p)
-	path.invert()
-
-	print(path)
-
-	set_process(true)
-
-	if (draw_path):
-
-		print("began drawing path")
-
-		var im = get_node("draw")
-		im.set_material_override(m)
-		im.clear()
-		im.begin(Mesh.PRIMITIVE_POINTS, null)
-		for x in p:
-			im.add_vertex(x)
-		im.end()
-
-func _input(event):
-	if (event.is_class("InputEventMouseButton") and event.button_index == BUTTON_LEFT and event.pressed):
-
-		print("detected mouse left button press")
-
-		var from = get_node("cambase/Camera").project_ray_origin(event.position)
-		var to = from + get_node("cambase/Camera").project_ray_normal(event.position) * 100
-		var p = get_closest_point_to_segment(from, to)
-
-		begin = get_closest_point(get_node("person_base").get_translation())
-		end = p
-
-		_update_path()
-
-	if (event.is_class("InputEventMouseMotion")):
-		if (event.button_mask&(BUTTON_MASK_MIDDLE+BUTTON_MASK_RIGHT)):
-			camrot += event.relative.x * 0.005
-			get_node("cambase").set_rotation(Vector3(0, camrot, 0))
-			print("camrot ", camrot)
-
-func _ready():
-	set_process_input(true)
-
-	m.flags_unshaded = true
-	m.flags_use_point_size = true
-	m.albedo_color = Color(1.0, 1.0, 1.0, 1.0)
+func updatePath(p_begin, p_end):
+	var p = get_simple_path(p_begin, p_end, true)
+	var p_path = Array(p)
+	p_path.invert()
+	return p_path
--- person_base.gd
+++ person_base.gd
@@ -1,12 +1,127 @@
 extends Position3D
 
+onready var navNodeRef = get_parent().get_parent()#  gets the navigation node
+
 var state = 0
 var speed = 4.0
 
-###
+var begin = Vector3()
+var end = Vector3()
+var path = Array()
+
+func go_here(pos):#  called to set up a path to follow
+	# clears previous values
+	begin = Vector3()
+	end = Vector3()
+	path = Array()
+	
+	begin = navNodeRef.getBegin(self)#  sets up begin
+	end = navNodeRef.getEnd(pos)#  sets up end
+	path = navNodeRef.updatePath(begin, end)#  updates the path
+	
+	#  at this point, the path has been set up.
+	#  now, call go_step to move the character
+	#  by a step
+
+func go_step(processDelta): 
+	#  actually moves the character by a step
+	
+	if path.size() > 1:
+		var toWalk = processDelta * speed
+		var toWatch = Vector3(0, 1, 0)
+		
+		while toWalk > 0 and path.size() >= 2:
+			var pFrom = path[path.size() - 1]
+			var pTo = path[path.size() - 2]
+			
+			toWatch = (pTo - pFrom).normalized()
+			
+			var d = pFrom.distance_to(pTo)
+			if d <= toWalk:
+				path.remove(path.size() - 1)
+				toWalk -= d
+			else:
+				path[path.size() - 1] = pFrom.linear_interpolate(pTo, toWalk/d)
+				toWalk = 0
+		
+		var atPos = path[path.size() - 1]
+		var atDir = toWatch
+		atDir.y = 0
+		
+		var t = Transform()
+		
+		t.origin = atPos
+		t = t.looking_at(atPos + atDir, Vector3(0, 1, 0))#  this makes the character face where they are going
+		self.set_transform(t)
+		
+		if path.size() < 2:
+			path = []
+			#  if this point is reached, our destination has been arrived at
+		
+	else:
+		#  this means we are already at our destination
+		pass
+
+#  the following calls the above, but uses the Position3Ds 
+#  go_pos_z, go_neg_z, go_neg_x, go_pos_x to just move in
+#  a direction;  could also be used for player control
+func go_up(processDelta):
+	begin = Vector3()
+	end = Vector3()
+	path = Array()
+	
+	begin = navNodeRef.getBegin(self)
+	end = navNodeRef.pointGetEnd(Vector3(self.translation.x, self.translation.y, (self.translation.z - 2)))
+	path = navNodeRef.updatePath(begin, end)
+	
+	go_step(processDelta)
+
+func go_down(processDelta):
+	begin = Vector3()
+	end = Vector3()
+	path = Array()
+	
+	begin = navNodeRef.getBegin(self)
+	end = navNodeRef.pointGetEnd(Vector3(self.translation.x, self.translation.y, (self.translation.z + 2)))
+	path = navNodeRef.updatePath(begin, end)
+	
+	go_step(processDelta)
+
+func go_left(processDelta):
+	begin = Vector3()
+	end = Vector3()
+	path = Array()
+	
+	begin = navNodeRef.getBegin(self)
+	end = navNodeRef.pointGetEnd(Vector3((self.translation.x + 2), self.translation.y, self.translation.z))
+	path = navNodeRef.updatePath(begin, end)
+	
+	go_step(processDelta)
+
+func go_right(processDelta):
+	begin = Vector3()
+	end = Vector3()
+	path = Array()
+	
+	begin = navNodeRef.getBegin(self)
+	end = navNodeRef.pointGetEnd(Vector3((self.translation.x - 2), self.translation.y, self.translation.z))
+	path = navNodeRef.updatePath(begin, end)
+	
+	go_step(processDelta)
 
 func _process(delta):
 	
+	#tests directional movement with player input
+	if Input.is_action_pressed("ui_up"):#  up
+		go_up(delta)
+	elif Input.is_action_pressed("ui_down"):#  down
+		go_down(delta)
+	elif Input.is_action_pressed("ui_left"):#  left
+		go_left(delta)
+	elif Input.is_action_pressed("ui_right"):#  right
+		go_right(delta)
+	
+	#deals with states
 	var current_action = 0
 	var completed_action = true
 	
