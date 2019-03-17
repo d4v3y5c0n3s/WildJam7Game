9ad4e79ba358a9d66cf4f8b01434884bfe19a3a8
pobber22 <48448414+pobber22@users.noreply.github.com>
2019-03-16 21:27:50 -0400

    updated camera and possession mechanic

--- level_navigation.gd
+++ level_navigation.gd
@@ -1,5 +1,16 @@
 extends Navigation
 
+# == CAMERA STUFF ==
+onready var currentchar = $person
+var hasCamera = false
+
+var ppl # List of people.
+var pIndex = 0 # Index in the people list.
+
+onready var camera = $cambase/Camera
+
+
+
 func getBegin(person_ref):
 	print(get_closest_point(person_ref.get_translation()))
 	return get_closest_point(person_ref.get_translation())
@@ -17,3 +28,34 @@ func updatePath(p_begin, p_end):
 	p_path.invert()
 	print(p_path)
 	return p_path
+	
+# == CAMERA FUNCTIONS ==
+	
+func possess(): #Possess
+	for person in ppl: # check every node in list
+		person.possessed = false #un-possess them
+		
+	# For now it cycles through in one direction, this could probably change
+	pIndex += 1 
+	if pIndex > len(ppl):
+		pIndex = 1
+	if $people.has_node("person"+str(pIndex)): # if the person exists 
+		currentchar = $people.get_child(pIndex-1) # set the current character
+		currentchar.possessed = true # possess him/her
+		currentchar.go_here(currentchar.translation) # stop him/her in their tracks
+		hasCamera = true # update the camera every frame
+	
+
+func _ready():
+	
+	ppl = $people.get_children()
+	
+
+func _process(delta):
+	if hasCamera: 
+		camera.translation = Vector3(currentchar.translation.x, currentchar.translation.y + 5, 50)
+		#print(camera.translation)
+		
+	if Input.is_action_just_pressed("ui_accept"): #possess with button
+		possess()
+		
